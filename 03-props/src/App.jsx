cf3095117c7976335bcd312e4861c725ebe44a3c
Divy Dev Jaiswal <work.divydev@gmail.com>
2026-08-18 18:18:02 +0530

    Add props and Cine-Max projects

---
+++ 03-props/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='parent'>
+    <div className='card'> 
+      <img src='https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FybHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='card' />
+      <h1>divy dev</h1>
+      <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+      <button>Click me</button>
+    </div>
+    <div className='card'> 
+      <img src='https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FybHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='card' />
+      <h1>divy dev</h1>
+      <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+      <button>Click me</button>
+    </div>
+    <div className='card'> 
+      <img src='https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FybHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='card' />
+      <h1>divy dev</h1>
+      <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.</p>
+      <button>Click me</button>
+    </div>
+    </div>
+  )
+}
+
+export default App
